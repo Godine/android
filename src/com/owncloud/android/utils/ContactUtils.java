c57b7a30fd2cdcda825cd2949d9587fe1d3df1bc
Jerome Van Der Linden <jeromevdl@gmail.com>
2015-07-09 17:59:08 +0200

    clean ops each time

--- src/com/owncloud/android/utils/ContactUtils.java
+++ src/com/owncloud/android/utils/ContactUtils.java
@@ -92,10 +92,10 @@ public class ContactUtils {
     public static void duplicateContactsForAccount(final Context context, final List<Contact> contacts, final Account currentAccount) {
         Log.i("Synchro", "Synchronizing " + contacts.size() + " contacts int account " + currentAccount.name);
 
-        ArrayList<ContentProviderOperation> ops;
+        ArrayList<ContentProviderOperation> ops = new ArrayList<>();
 
         for (final Contact contact : contacts) {
-            ops = new ArrayList<>();
+            ops.clear();
             Log.i("Synchro", "Synchronize " + contact.getName() + " start");
             ops.add(ContentProviderOperation.newInsert(
                     RawContacts.CONTENT_URI)
